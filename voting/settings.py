6579cc60069cbb6b79ebcd69bd3aa64da9ceb6a4
Christian Pönisch <christian.poenisch@tracetronic.de>
2020-09-24 19:31:35 +0200

    Auto-generate secret key if not passed

--- voting/settings.py
+++ voting/settings.py
@@ -5,11 +5,13 @@ Django settings for voting project.
 import os
 import dj_database_url
 
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
